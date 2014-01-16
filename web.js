c676c59256f88f0dcd3e28529aaa65dac5704d60
Julian Fitzell <julian.fitzell@guardian.co.uk>
2014-01-16 15:19:11 +0000

    Have a WebSocket server on 5000 and a HTTP server on PORT that can send a message

--- web.js
+++ web.js
@@ -1,19 +1,39 @@
 var WebSocketServer = require('ws').Server
   , http = require('http')
   , express = require('express')
-  , app = express()
-  , port = process.env.PORT || 5000;
+  , wsApp = express()
+  , wsPort = 5000
+  , httpApp = express()
+  , httpPort = process.env.PORT || 80;
 
-app.use(express.static(__dirname + '/'));
+// Set up WebSocket server
+//wsApp.use(express.static(__dirname + '/'));
 
-var server = http.createServer(app);
-server.listen(port);
+var wsServer = http.createServer(wsApp);
+wsServer.listen(wsPort);
 
-console.log('http server listening on %d', port);
+console.log('Web socket server listening on %d', wsPort);
 
-var wss = new WebSocketServer({server: server});
-console.log('websocket server created');
+
+// Set up plain HTTP server
+httpApp.use(express.static(__dirname + '/'));
+
+var httpServer = http.createServer(httpApp);
+httpServer.listen(httpPort);
+
+console.log('http server listening on %d', httpPort);
+
+
+
+
+var sockets = new Array();
+
+var wss = new WebSocketServer({server: wsServer});
+console.log('websocket wsServer created');
 wss.on('connection', function(ws) {
+	sockets.push(ws);
+	console.log('Pushed new socket. List size: ' + sockets.length);
+	
     var id = setInterval(function() {
         ws.send(JSON.stringify(new Date()), function() { });
     }, 10000);
@@ -23,5 +43,20 @@ wss.on('connection', function(ws) {
     ws.on('close', function() {
         console.log('websocket connection close');
         clearInterval(id);
+        
+        var index = sockets.indexOf(ws);
+        if (index > -1) {
+        	sockets.splice(index, 1);
+        }
+        console.log('Removed socket. List size: ' + sockets.length);
     });
+});
+
+
+httpApp.get('/', function(req, res) {
+	sockets.map(function(each) {
+		each.send(JSON.stringify(req.query.message), function() { });
+	});
+	
+	res.send('Message sent!');
 });
