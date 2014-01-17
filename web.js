bd6a8a69168ddd038ad745c9b90f1331e2576f69
alastairjardine <alastair@11-Deacon-Roads-MacBook-Pro.local>
2014-01-17 11:01:46 +0000

    Added soulmates to the handlers etc

--- web.js
+++ web.js
@@ -77,6 +77,13 @@ function Message(subject, text) {
 	this.text = text;
 }
 
+function SoulmatesDM(sendername, text) {
+	this.type = 'soulmatesDM';
+	this.sender = sendername;
+	this.text = text;
+}
+
+
 function Keepalive() {
 	this.type = 'keepalive';
 }
@@ -204,6 +211,8 @@ app.get('/comment', function(req, res) {
 	res.send('Notification sent!');
 });
 
+// create a get endpoint to test with
+
 app.get('/send', function(req, res) {
 	if (req.query.message) {
 		sendNotification(new Message(req.query.message));
@@ -211,4 +220,9 @@ app.get('/send', function(req, res) {
 	} else {
 		res.send(400, 'No message specified');
 	}
+});
+
+app.get('/soulmates', function(req, res) {
+	sendNotification(new SoulmatesDM('hotlips34', 'Hey. Saw your profile on the Soulmates homepage, and thought you looked someone I might like to meet'));
+	res.send('Message sent!');
 });
